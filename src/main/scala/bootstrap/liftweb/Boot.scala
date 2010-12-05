54dc9eb3a66493a2c33d8b705fc70a1254acf0ad
philip142au <philip_andrew@hotmail.com>
2010-12-05 21:48:21 +0800

    test

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -26,11 +26,16 @@ class Boot {
     LiftRules.addToPackages("code")
 
     // Build SiteMap
+    /*
     val entries = Menu(Loc("Home", List("index"), "Home")) ::
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")) ::
-      Menu(Loc("Pet Form", Link(List("petform"), true, "/petform"), "Pet Form")) :: User.sitemap
+      Menu(Loc("Pets", Link(List("pet"), true, "/pet"), "Pets")) ::
+      Menu(Loc("Pet edit", Link(List("petedit"), false, "/pet/edit"), "Pet edit")) ::
+      User.sitemap
+    */
 
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    //LiftRules.setSiteMap(SiteMap(entries:_*))
+    LiftRules.setSiteMapFunc(MenuInfo.sitemap)
 
     /*
     * Show the spinny image when an Ajax call starts
@@ -60,4 +65,61 @@ class Boot {
 
   private def localeCalculator(request : Box[HTTPRequest]): Locale =
     User.currentUser.map(u => new Locale(u.locale.value)) openOr Locale.getDefault
+}
+
+
+object MenuInfo {
+import Loc._
+
+  def sitemap() = SiteMap(
+    Menu("Home") / "index",
+    Menu("Pet") / "pet" submenus(
+      Menu("Edit Pet") / "pet" / "edit")
+      )
+  /*
+      Menu("Ajax Samples") / "ajax",
+      Menu("Ajax Form") / "ajax-form",
+      Menu("Modal Dialog") / "rhodeisland",
+      Menu("JSON Messaging") / "json",
+      Menu("Stateless JSON Messaging") / "stateless_json",
+      Menu("More JSON") / "json_more",
+      Menu("Ajax and Forms") / "form_ajax") ,
+    Menu("Persistence") / "persistence" >> noGAE submenus (
+      Menu("XML Fun") / "xml_fun" >> noGAE,
+      Menu("Database") / "database" >> noGAE,
+      Menu(Loc("simple", Link(List("simple"), true, "/simple/index"), "Simple Forms", noGAE)),
+      Menu("Templates") / "template" >> noGAE),
+    Menu("Templating") / "templating" / "index" submenus(
+      Menu("Surround") / "templating" / "surround",
+      Menu("Embed") / "templating" / "embed",
+      Menu("Evalutation Order") / "templating" / "eval_order",
+      Menu("Select <div>s") / "templating" / "selectomatic",
+      Menu("Simple Wizard") / "simple_wizard",
+      Menu("Lazy Loading") / "lazy",
+      Menu("Parallel Snippets") / "parallel",
+      Menu("<head/> tag") / "templating"/ "head"),
+    Menu("Web Services") / "ws" >> noGAE,
+    Menu("Localization") / "lang",
+    Menu("Menus") / "menu" / "index" submenus(
+      Menu("First Submenu") / "menu" / "one",
+      Menu("Second Submenu (has more)") / "menu" / "two" submenus(
+        Menu("First (2) Submenu") / "menu" / "two_one",
+        Menu("Second (2) Submenu") / "menu" / "two_two"),
+      Menu("Third Submenu") / "menu" / "three",
+      Menu("Forth Submenu") / "menu" / "four"),
+    Menu(WikiStuff),
+    Menu("Misc code") / "misc" submenus(
+      Menu("Number Guessing") / "guess",
+      Menu("Wizard") / "wiz",
+      Menu("Wizard Challenge") / "wiz2",
+      Menu("Simple Screen") / "simple_screen",
+      Menu("Variable Screen") / "variable_screen",
+      Menu("Arc Challenge #1") / "arc",
+      Menu("File Upload") / "file_upload",
+      Menu(Loc("login", Link(List("login"), true, "/login/index"),
+               <xml:group>Requiring Login<strike>SiteMap</strike> </xml:group>)),
+      Menu("Counting") / "count"),
+    Menu(Loc("lift", ExtLink("http://liftweb.net"),
+             <xml:group> <i>Lift</i>project home</xml:group>)))*/
+
 }
